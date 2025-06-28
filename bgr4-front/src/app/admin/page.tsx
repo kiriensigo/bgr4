1f204fda2b30400cac3025e291db596d0182ba83
kiriensigo <kiriensigo@gmail.com>
2025-06-28 19:15:02 +0900

    fix build2

--- bgr4-front/src/app/admin/page.tsx
+++ bgr4-front/src/app/admin/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { useAuth } from "@/contexts/AuthContext";
+import { useAuth } from "../../contexts/AuthContext";
 import {
   Container,
   Typography,
